059193994b86a99f358dea3e69ca385e622d90ba
Alexandr <35874956+lexnnov@users.noreply.github.com>
2019-04-23 15:27:47 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -20,8 +20,8 @@ export default {
   },
   methods: {
     async fetchSomething() {
-      const ip = await this.$axios.$post('http://krapipl.imumk.ru:8082/api/mobilev1/update', {'data': ''})
-      this.$store.commit('items/addCourses', ip.items)
+      const res = await this.$axios.$post('http://krapipl.imumk.ru:8082/api/mobilev1/update', {'data': ''})
+      this.$store.commit('items/addCourses', res.items)
     }
   }
 
